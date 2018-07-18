e9c5fdf014f2b89e29c2b11e12b59a70a8cefbf6
xyy <xyy-JAVA@outlook.com>
2018-07-18 14:54:09 +0800

    update pom.xml

--- MyBoot-hystrix/src/main/java/com/xyy/Application.java
+++ MyBoot-hystrix/src/main/java/com/xyy/Application.java
@@ -1,12 +1,10 @@
 package com.xyy;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Application
