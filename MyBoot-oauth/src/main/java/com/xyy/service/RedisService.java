8d7abca8482814d3d5dbd9dc78b286e7dd1cc370
xyy <xyy-JAVA@outlook.com>
2018-07-20 10:13:44 +0800

    update redisservice

--- MyBoot-oauth/src/main/java/com/xyy/service/RedisService.java
+++ MyBoot-oauth/src/main/java/com/xyy/service/RedisService.java
@@ -12,17 +12,51 @@ import java.util.List;
  * The word 'impossible' is not in my dictionary.
  */
 public interface RedisService {
+    /**
+     * 保存键key，值value的数据
+     * @param key
+     * @param value
+     * @return
+     */
     boolean set(String key, String value);
 
+    /**
+     * 根据键获取Redis中的值
+     * @param key
+     * @return
+     */
     String get(String key);
 
+    /**
+     * 设定键的过期时间
+     * @param key
+     * @param expire
+     * @return
+     */
     boolean expire(String key, long expire);
 
     <T> boolean setList(String key, List<T> list);
 
+    /**
+     * 在key对应list的头部添加字符串元素
+     * @param key
+     * @param obj
+     * @return
+     */
     long lpush(String key, Object obj);
 
+    /**
+     * 在key对应list的尾部添加字符串元素
+     * @param key
+     * @param obj
+     * @return
+     */
     long rpush(String key, Object obj);
 
+    /**
+     * 移除并返回列表key的头元素
+     * @param key
+     * @return
+     */
     String lpop(String key);
 }
