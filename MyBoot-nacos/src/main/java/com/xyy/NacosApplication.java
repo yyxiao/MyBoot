9a465bddd8d4ac34f6f34d16a648989f96816c8d
Xander <xyy-JAVA@outlook.com>
2019-01-21 14:35:06 +0800

    add nacos

--- MyBoot-nacos/src/main/java/com/xyy/NacosApplication.java
+++ MyBoot-nacos/src/main/java/com/xyy/NacosApplication.java
@@ -4,9 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -25,4 +25,6 @@ public class NacosApplication {
 			return "hello " + name;
 		}
 	}
+
 }
+
